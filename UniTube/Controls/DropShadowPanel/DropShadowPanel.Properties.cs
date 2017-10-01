3df161c32125a3fa8da74b8a11c5d20c5e460460
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-01 02:49:24 -0300

    Added DropShadowPanel

---
+++ UniTube/Controls/DropShadowPanel/DropShadowPanel.Properties.cs
@@ -0,0 +1,159 @@
+﻿using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+
+namespace UniTube.Controls
+{
+    public partial class DropShadowPanel
+    {
+        public static readonly DependencyProperty BlurRadiusProperty =
+             DependencyProperty.Register(nameof(BlurRadius), typeof(double), typeof(DropShadowPanel), new PropertyMetadata(9.0, OnBlurRadiusChanged));
+
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(nameof(Color), typeof(Color), typeof(DropShadowPanel), new PropertyMetadata(Colors.Black, OnColorChanged));
+
+        public static readonly DependencyProperty OffsetXProperty =
+            DependencyProperty.Register(nameof(OffsetX), typeof(double), typeof(DropShadowPanel), new PropertyMetadata(0.0, OnOffsetXChanged));
+
+        public static readonly DependencyProperty OffsetYProperty =
+            DependencyProperty.Register(nameof(OffsetY), typeof(double), typeof(DropShadowPanel), new PropertyMetadata(0.0, OnOffsetYChanged));
+
+        public static readonly DependencyProperty OffsetZProperty =
+            DependencyProperty.Register(nameof(OffsetZ), typeof(double), typeof(DropShadowPanel), new PropertyMetadata(0.0, OnOffsetZChanged));
+
+        public static readonly DependencyProperty ShadowOpacityProperty =
+            DependencyProperty.Register(nameof(ShadowOpacity), typeof(double), typeof(DropShadowPanel), new PropertyMetadata(1.0, OnShadowOpacityChanged));
+
+        public DropShadow DropShadow => _dropShadow;
+
+        public CompositionBrush Mask
+        {
+            get
+            {
+                return _dropShadow?.Mask;
+            }
+
+            set
+            {
+                if (_dropShadow != null)
+                {
+                    _dropShadow.Mask = value;
+                }
+            }
+        }
+        
+        public double BlurRadius
+        {
+            get
+            {
+                return (double)GetValue(BlurRadiusProperty);
+            }
+
+            set
+            {
+                SetValue(BlurRadiusProperty, value);
+            }
+        }
+        
+        public Color Color
+        {
+            get
+            {
+                return (Color)GetValue(ColorProperty);
+            }
+
+            set
+            {
+                SetValue(ColorProperty, value);
+            }
+        }
+        
+        public double OffsetX
+        {
+            get
+            {
+                return (double)GetValue(OffsetXProperty);
+            }
+
+            set
+            {
+                SetValue(OffsetXProperty, value);
+            }
+        }
+        
+        public double OffsetY
+        {
+            get
+            {
+                return (double)GetValue(OffsetYProperty);
+            }
+
+            set
+            {
+                SetValue(OffsetYProperty, value);
+            }
+        }
+        
+        public double OffsetZ
+        {
+            get
+            {
+                return (double)GetValue(OffsetZProperty);
+            }
+
+            set
+            {
+                SetValue(OffsetZProperty, value);
+            }
+        }
+        
+        public double ShadowOpacity
+        {
+            get
+            {
+                return (double)GetValue(ShadowOpacityProperty);
+            }
+
+            set
+            {
+                SetValue(ShadowOpacityProperty, value);
+            }
+        }
+
+        private static void OnBlurRadiusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var panel = d as DropShadowPanel;
+            panel?.OnBlurRadiusChanged((double)e.NewValue);
+        }
+
+        private static void OnColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var panel = d as DropShadowPanel;
+            panel?.OnColorChanged((Color)e.NewValue);
+        }
+
+        private static void OnOffsetXChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var panel = d as DropShadowPanel;
+            panel?.OnOffsetXChanged((double)e.NewValue);
+        }
+
+        private static void OnOffsetYChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var panel = d as DropShadowPanel;
+            panel?.OnOffsetYChanged((double)e.NewValue);
+        }
+
+        private static void OnOffsetZChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var panel = d as DropShadowPanel;
+            panel?.OnOffsetZChanged((double)e.NewValue);
+        }
+
+        private static void OnShadowOpacityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var panel = d as DropShadowPanel;
+            panel?.OnShadowOpacityChanged((double)e.NewValue);
+        }
+    }
+}
---
+++ UniTube/Controls/DropShadowPanel/DropShadowPanel.cs
@@ -0,0 +1,181 @@
+﻿using System.Numerics;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Shapes;
+
+namespace UniTube.Controls
+{
+    [TemplatePart(Name = ShadowPart, Type = typeof(Border))]
+    public partial class DropShadowPanel : ContentControl
+    {
+        private const string ShadowPart = "Shadow";
+
+        private readonly DropShadow _dropShadow;
+        private readonly SpriteVisual _shadowVisual;
+        private Border _border;
+
+        public DropShadowPanel()
+        {
+            DefaultStyleKey = typeof(DropShadowPanel);
+
+            Compositor compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+
+            _shadowVisual = compositor.CreateSpriteVisual();
+
+            _dropShadow = compositor.CreateDropShadow();
+            _shadowVisual.Shadow = _dropShadow;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            _border = GetTemplateChild(ShadowPart) as Border;
+
+            if (_border != null)
+            {
+                ElementCompositionPreview.SetElementChildVisual(_border, _shadowVisual);
+            }
+
+            ConfigureShadowVisualForCastingElement();
+
+            base.OnApplyTemplate();
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            if (oldContent != null)
+            {
+                var oldElement = oldContent as FrameworkElement;
+
+                if (oldElement != null)
+                {
+                    oldElement.SizeChanged -= OnSizeChanged;
+                }
+            }
+
+            if (newContent != null)
+            {
+                var newElement = newContent as FrameworkElement;
+
+                if (newElement != null)
+                {
+                    newElement.SizeChanged += OnSizeChanged;
+                }
+            }
+
+            base.OnContentChanged(oldContent, newContent);
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateShadowSize();
+        }
+
+        private void ConfigureShadowVisualForCastingElement()
+        {
+            UpdateShadowMask();
+
+            UpdateShadowSize();
+        }
+
+        private void OnBlurRadiusChanged(double newValue)
+        {
+            if (_dropShadow != null)
+            {
+                _dropShadow.BlurRadius = (float)newValue;
+            }
+        }
+
+        private void OnColorChanged(Color newValue)
+        {
+            if (_dropShadow != null)
+            {
+                _dropShadow.Color = newValue;
+            }
+        }
+
+        private void OnOffsetXChanged(double newValue)
+        {
+            if (_dropShadow != null)
+            {
+                UpdateShadowOffset((float)newValue, _dropShadow.Offset.Y, _dropShadow.Offset.Z);
+            }
+        }
+
+        private void OnOffsetYChanged(double newValue)
+        {
+            if (_dropShadow != null)
+            {
+                UpdateShadowOffset(_dropShadow.Offset.X, (float)newValue, _dropShadow.Offset.Z);
+            }
+        }
+
+        private void OnOffsetZChanged(double newValue)
+        {
+            if (_dropShadow != null)
+            {
+                UpdateShadowOffset(_dropShadow.Offset.X, _dropShadow.Offset.Y, (float)newValue);
+            }
+        }
+
+        private void OnShadowOpacityChanged(double newValue)
+        {
+            if (_dropShadow != null)
+            {
+                _dropShadow.Opacity = (float)newValue;
+            }
+        }
+
+        private void UpdateShadowMask()
+        {
+            if (Content != null)
+            {
+                CompositionBrush mask = null;
+
+                if (Content is Image)
+                {
+                    mask = ((Image)Content).GetAlphaMask();
+                }
+                else if (Content is Shape)
+                {
+                    mask = ((Shape)Content).GetAlphaMask();
+                }
+                else if (Content is TextBlock)
+                {
+                    mask = ((TextBlock)Content).GetAlphaMask();
+                }
+
+                _dropShadow.Mask = mask;
+            }
+            else
+            {
+                _dropShadow.Mask = null;
+            }
+        }
+
+        private void UpdateShadowOffset(float x, float y, float z)
+        {
+            if (_dropShadow != null)
+            {
+                _dropShadow.Offset = new Vector3(x, y, z);
+            }
+        }
+
+        private void UpdateShadowSize()
+        {
+            if (_shadowVisual != null)
+            {
+                Vector2 newSize = new Vector2(0, 0);
+                FrameworkElement contentFE = Content as FrameworkElement;
+                if (contentFE != null)
+                {
+                    newSize = new Vector2((float)contentFE.ActualWidth, (float)contentFE.ActualHeight);
+                }
+
+                _shadowVisual.Size = newSize;
+            }
+        }
+    }
+}
