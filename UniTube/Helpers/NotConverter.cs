0018717cc90dc83c7c1480f1ae5e69a25544923a
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 11:58:50 -0300

    Added NotConverter and SelectedItemConverter

---
+++ UniTube/Helpers/NotConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Windows.UI.Xaml.Data;
+
+namespace UniTube.Helpers
+{
+    public class NotConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => !(bool)value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => !(bool)value;
+    }
+}
---
+++ UniTube/Helpers/SelectedItemConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UniTube.Helpers
+{
+    public class SelectedItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string)
+                return (string)value == (string)parameter;
+            else if (value is ElementTheme)
+                return ((ElementTheme)value).ToString() == (string)parameter;
+
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => throw new NotImplementedException();
+    }
+}
