0a8b82e7d85946fdb4479afe7343f4f9502094b0
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 11:50:00 -0300

    Toolbar.cs refactored

--- UniTube/Controls/Toolbar/Toolbar.cs
+++ UniTube/Controls/Toolbar/Toolbar.cs
@@ -5,11 +5,11 @@ namespace UniTube.Controls
 {
     [TemplateVisualState(GroupName = "AutoSuggestStates", Name = AutoSuggestBoxVisibleState)]
     [TemplateVisualState(GroupName = "AutoSuggestStates", Name = AutoSuggestBoxCollapsedState)]
+    [TemplateVisualState(GroupName = "HeaderStates", Name = HeaderVisibleState)]
+    [TemplateVisualState(GroupName = "HeaderStates", Name = HeaderCollapsedState)]
     [TemplateVisualState(GroupName = "ModeStates", Name = ModeLargeState)]
     [TemplateVisualState(GroupName = "ModeStates", Name = ModeCompactClosedState)]
     [TemplateVisualState(GroupName = "ModeStates", Name = ModeCompactNotClosedState)]
-    [TemplateVisualState(GroupName = "HeaderStates", Name = HeaderVisibleState)]
-    [TemplateVisualState(GroupName = "HeaderStates", Name = HeaderCollapsedState)]
     [TemplateVisualState(GroupName = "TogglePaneStates", Name = TogglePaneButtonVisibleState)]
     [TemplateVisualState(GroupName = "TogglePaneStates", Name = TogglePaneButtonCollapsedState)]
     [TemplatePart(Name = AutoSuggestButtonPart, Type = typeof(ToolbarButton))]
@@ -17,29 +17,35 @@ namespace UniTube.Controls
     [TemplatePart(Name = PaneToggleButtonPart, Type = typeof(ToolbarButton))]
     public partial class Toolbar : ContentControl
     {
-        private const string AutoSuggestBoxVisibleState = "AutoSuggestBoxVisible";
-        private const string AutoSuggestBoxCollapsedState = "AutoSuggestBoxCollapsed";
-        private const string ModeLargeState = "ModeLarge";
-        private const string ModeCompactClosedState = "ModeCompactClosed";
-        private const string ModeCompactNotClosedState = "ModeCompactNotClosed";
-        private const string HeaderVisibleState = "HeaderVisible";
-        private const string HeaderCollapsedState = "HeaderCollapsed";
-        private const string AutoSuggestButtonPart = "AutoSuggestButton";
-        private const string AutoSuggestBoxPresenterPart = "AutoSuggestBoxPresenter";
-        private const string PaneToggleButtonPart = "PaneToggleButton";
-        private const string TogglePaneButtonVisibleState = "TogglPaneButtonVisible";
+        private const string AutoSuggestBoxCollapsedState   = "AutoSuggestBoxCollapsed";
+        private const string AutoSuggestBoxVisibleState     = "AutoSuggestBoxVisible";
+        private const string HeaderCollapsedState           = "HeaderCollapsed";
+        private const string HeaderVisibleState             = "HeaderVisible";
+        private const string ModeCompactClosedState         = "ModeCompactClosed";
+        private const string ModeCompactNotClosedState      = "ModeCompactNotClosed";
+        private const string ModeLargeState                 = "ModeLarge";
         private const string TogglePaneButtonCollapsedState = "TogglePaneButtonCollapsed";
+        private const string TogglePaneButtonVisibleState   = "TogglePaneButtonVisible";
+        private const string AutoSuggestBoxPresenterPart    = "AutoSuggestBoxPresenter";
+        private const string AutoSuggestButtonPart          = "AutoSuggestButton";
+        private const string PaneToggleButtonPart           = "PaneToggleButton";
 
-        private ToolbarButton _autoSuggestButton;
-        private ContentControl _autoSuggestBoxPresenter;
-        private ToolbarButton _paneToggleButton;
+        private ToolbarButton   _autoSuggestButton;
+        private ContentControl  _autoSuggestBoxPresenter;
+        private ToolbarButton   _paneToggleButton;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Toolbar"/> class.
+        /// </summary>
         public Toolbar()
         {
             DefaultStyleKey = typeof(Toolbar);
             SizeChanged += OnSizeChanged;
         }
 
+        /// <summary>
+        /// Occurs when you click the pane toggle button.
+        /// </summary>
         public event RoutedEventHandler PaneToggleButtonClick;
 
         protected override void OnApplyTemplate()
@@ -54,45 +60,15 @@ namespace UniTube.Controls
             OnIsPaneToggleButtonVisibleChanged(IsPaneToggleButtonVisible);
         }
 
-        private void OnAutoSuggestButtonClick(object sender, RoutedEventArgs e)
-        {
-            IsAutoSuggestBoxActivated = true;
-        }
+        private void OnAutoSuggestButtonClick(object sender, RoutedEventArgs e) => IsAutoSuggestBoxActivated = true;
+
+        private void OnAutoSuggestBoxChanged(AutoSuggestBox newValue)
+            => VisualStateManager.GoToState(this, AutoSuggestBox == null ? AutoSuggestBoxCollapsedState : AutoSuggestBoxVisibleState, false);
 
         private void OnAutoSuggestBoxPresenterLostFocus(object sender, RoutedEventArgs e)
         {
-            IsAutoSuggestBoxActivated = false;
-        }
-
-        private void OnPaneToggleButtonClick(object sender, RoutedEventArgs e)
-        {
-            PaneToggleButtonClick?.Invoke(this, new RoutedEventArgs());
-
-            if (PaneToggleButtonCommand.CanExecute(null))
-            {
-                PaneToggleButtonCommand.Execute(null);
-            }
-        }
-
-        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (e.NewSize.IsEmpty) return;
-
-            IsAutoSuggestBoxActivated = false;
-
-            if (e.NewSize.Width >= LargeModeThresholdWidth)
-            {
-                Mode = ToolbarMode.Large;
-            }
-            else
-            {
-                Mode = ToolbarMode.Compact;
-            }
-        }
-
-        private void OnAutoSuggestBoxChanged(AutoSuggestBox newValue)
-        {
-            VisualStateManager.GoToState(this, AutoSuggestBox == null ? AutoSuggestBoxCollapsedState : AutoSuggestBoxVisibleState, false);
+            if (!IsSearchModeEnabled)
+                IsAutoSuggestBoxActivated = false;
         }
 
         private void OnIsAutoSuggestBoxActivatedChanged(bool newValue)
@@ -102,14 +78,32 @@ namespace UniTube.Controls
                 AutoSuggestBoxPresenter.Focus(FocusState.Programmatic);
         }
 
+        private void OnModeChanged(ToolbarMode newValue)
+            => UpdateMode(AlwaysShowHeader, IsAutoSuggestBoxActivated, newValue);
+
         private void OnIsPaneToggleButtonVisibleChanged(bool newValue)
+            => VisualStateManager.GoToState(this, newValue ? TogglePaneButtonVisibleState : TogglePaneButtonCollapsedState, false);
+
+        private void OnIsSearchModeEnabledChanged(bool newValue) => IsAutoSuggestBoxActivated = newValue;
+
+        private void OnPaneToggleButtonClick(object sender, RoutedEventArgs e)
         {
-            VisualStateManager.GoToState(this, newValue ? TogglePaneButtonVisibleState : TogglePaneButtonCollapsedState, false);
+            PaneToggleButtonClick?.Invoke(this, new RoutedEventArgs());
+
+            if ((PaneToggleButtonCommand?.CanExecute(null)).GetValueOrDefault())
+                PaneToggleButtonCommand?.Execute(null);
         }
 
-        private void OnModeChanged(ToolbarMode newValue)
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            UpdateMode(AlwaysShowHeader, IsAutoSuggestBoxActivated, newValue);
+            if (e.NewSize.IsEmpty) return;
+
+            IsAutoSuggestBoxActivated = IsSearchModeEnabled;
+
+            if (e.NewSize.Width >= LargeModeThresholdWidth)
+                Mode = ToolbarMode.Large;
+            else
+                Mode = ToolbarMode.Compact;
         }
 
         private void UpdateMode(bool alwaysShowHeader, bool isAutoSuggestBoxActivated, ToolbarMode mode)
