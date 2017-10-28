80540c009be24668eef65403973fc02e276d3710
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 19:26:49 -0300

    Added ViewModel for Saved and History page to fix an issue with navigation 😋

--- UniTube/Views/HistoryPage.xaml.cs
+++ UniTube/Views/HistoryPage.xaml.cs
@@ -1,9 +1,13 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using UniTube.ViewModels;
+
+using Windows.UI.Xaml.Controls;
 
 namespace UniTube.Views
 {
     public sealed partial class HistoryPage : Page
     {
+        private HistoryViewModel ViewModel => (DataContext as HistoryViewModel);
+
         public HistoryPage() => InitializeComponent();
     }
 }
--- UniTube/Views/SavedPage.xaml.cs
+++ UniTube/Views/SavedPage.xaml.cs
@@ -1,9 +1,13 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using UniTube.ViewModels;
+
+using Windows.UI.Xaml.Controls;
 
 namespace UniTube.Views
 {
     public sealed partial class SavedPage : Page
     {
+        private SavedViewModel ViewModel => (DataContext as SavedViewModel);
+
         public SavedPage() => InitializeComponent();
     }
 }
