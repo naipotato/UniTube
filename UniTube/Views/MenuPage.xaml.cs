d3bcfbbc414483e4847961ed8f823c785f183f4c
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 12:26:34 -0300

    Refactoring MenuPage.xaml.cs

--- UniTube/Views/MenuPage.xaml.cs
+++ UniTube/Views/MenuPage.xaml.cs
@@ -1,30 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using UniTube.ViewModels;
 
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Controls;
 
 namespace UniTube.Views
 {
-    /// <summary>
-    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
-    /// </summary>
     public sealed partial class MenuPage : Page
     {
-        public MenuPage()
-        {
-            this.InitializeComponent();
-        }
+        private MasterViewModel ViewModel => (DataContext as MasterViewModel);
+
+        public MenuPage() => InitializeComponent();
     }
 }
