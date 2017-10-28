20d0326e96f810b37c35b207cd28c05bca2b6064
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 03:11:21 -0300

    Added Search.cs and SearchResult.cs

---
+++ UniTube.Core/Resources/Search.cs
@@ -0,0 +1,14 @@
+﻿using UniTube.Core.Requests;
+
+namespace UniTube.Core.Resources
+{
+    public class Search
+    {
+        /// <summary>
+        /// Returns a collection of search results that match the query parameters specified in the API request.
+        /// </summary>
+        /// <param name="part">Specifies a comma-separated list of one or more <see cref="Search"/> resource
+        /// properties that the API response will include.</param>
+        public static SearchListRequest List(string part) => new SearchListRequest(part);
+    }
+}
---
+++ UniTube.Core/Resources/SearchResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UniTube.Core.Resources
+{
+    public class SearchResult
+    {
+        public string Kind { get; set; }
+        public string Etag { get; set; }
+        public SearchResultId Id { get; set; }
+        public SearchResultSnippet Snippet { get; set; }
+
+        public class SearchResultId
+        {
+            public string Kind { get; set; }
+            public string VideoId { get; set; }
+            public string ChannelId { get; set; }
+            public string PlaylistId { get; set; }
+        }
+
+        public class SearchResultSnippet
+        {
+            public DateTime PublishedAt { get; set; }
+            public string ChannelId { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public SearchResultThumbnails Thumbnails { get; set; }
+            public string ChannelTitle { get; set; }
+
+            public class SearchResultThumbnails
+            {
+                public Thumbnail Default { get; set; }
+                public Thumbnail Medium { get; set; }
+                public Thumbnail High { get; set; }
+            }
+        }
+    }
+}
