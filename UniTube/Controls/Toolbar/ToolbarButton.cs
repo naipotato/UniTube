3d0fb7d3f69ced2717893cd85841f60e84fb40f4
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 11:51:11 -0300

    Refactoring ToolbarButton and ToolbarMode

--- UniTube/Controls/Toolbar/ToolbarButton.cs
+++ UniTube/Controls/Toolbar/ToolbarButton.cs
@@ -4,9 +4,6 @@ namespace UniTube.Controls
 {
     public class ToolbarButton : Button, IToolbarElement
     {
-        public ToolbarButton()
-        {
-            DefaultStyleKey = typeof(ToolbarButton);
-        }
+        public ToolbarButton() => DefaultStyleKey = typeof(ToolbarButton);
     }
 }
--- UniTube/Controls/Toolbar/ToolbarMode.cs
+++ UniTube/Controls/Toolbar/ToolbarMode.cs
@@ -2,7 +2,14 @@
 {
     public enum ToolbarMode
     {
+        /// <summary>
+        /// 
+        /// </summary>
         Compact = 0,
+
+        /// <summary>
+        /// 
+        /// </summary>
         Large = 1
     }
 }
