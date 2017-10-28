fcfed1c45b6aeb282a2c9056bb5fd6319a509bc9
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 11:57:23 -0300

    Json.cs refactored

--- UniTube/Helpers/Json.cs
+++ UniTube/Helpers/Json.cs
@@ -6,20 +6,10 @@ namespace UniTube.Helpers
 {
     public static class Json
     {
-        public static async Task<T> ToObjectAsync<T>(string value)
-        {
-            return await Task.Run(() =>
-            {
-                return JsonConvert.DeserializeObject<T>(value);
-            });
-        }
+        public static async Task<T> ToObjectAsync<T>(string value) => await Task.Run(()
+            => JsonConvert.DeserializeObject<T>(value));
 
-        public static async Task<string> StringifyAsync(object value)
-        {
-            return await Task.Run(() =>
-            {
-                return JsonConvert.SerializeObject(value);
-            });
-        }
+        public static async Task<string> StringifyAsync(object value) => await Task.Run(()
+            => JsonConvert.SerializeObject(value));
     }
 }
