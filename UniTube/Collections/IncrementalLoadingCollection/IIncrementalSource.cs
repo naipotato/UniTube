728972f0177a4244f662c2f0c3ecb24117642dfc
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-10-28 03:02:56 -0300

    Incremental collections implemented

---
+++ UniTube/Collections/IncrementalLoadingCollection/IIncrementalSource.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UniTube.Collections
+{
+    public interface IIncrementalSource<TSource>
+    {
+        Task<IEnumerable<TSource>> GetPagedItemsAsync(int pageIndex, int pageSize, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
