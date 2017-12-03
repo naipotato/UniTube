d1257901779da1fe499034775a1714aa7b55b9f3
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-12-03 19:31:02 -0300

    Removing code here to prepare another kind of implementation

--- UniTube/ViewModels/VideoViewModel.cs
+++ UniTube/ViewModels/VideoViewModel.cs
@@ -1,31 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
+﻿
 using Template10.Mvvm;
-
-using UniTube.Core.Resources;
-
-using Windows.UI.Xaml.Navigation;
 
 namespace UniTube.ViewModels
 {
     public class VideoViewModel : ViewModelBase
     {
-        private Video video;
 
-        public Video Video
-        {
-            get => video;
-            set => Set(ref video, value);
-        }
-
-        public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
-        {
-            if (parameter is Video)
-            {
-                Video = parameter as Video;
-            }
-            return Task.CompletedTask;
-        }
     }
 }
