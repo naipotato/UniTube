5a1f727e7765c597f296d791edbd8a66ee0b3388
nahuelwexd <nahual_gomca@outlook.com.ar>
2017-12-03 19:34:30 -0300

    Removed code behind event to use the viewmodel method

--- UniTube/Views/TrendingPage.xaml.cs
+++ UniTube/Views/TrendingPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using UniTube.Core.Resources;
 using UniTube.ViewModels;
-
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
@@ -15,13 +15,6 @@ namespace UniTube.Views
         {
             InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Enabled;
-        }
-
-        private void TrendingGridView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            var video = e.ClickedItem as Video;
-            TrendingGridView.PrepareConnectedAnimation("videoThumbnail", e.ClickedItem, "Thumbnail");
-            ViewModel.Locator.MasterViewModel.NavigationService.Navigate(Pages.Video, video, new SuppressNavigationTransitionInfo());
         }
     }
 }
