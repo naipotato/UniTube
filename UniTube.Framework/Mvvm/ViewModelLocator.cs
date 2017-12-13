ebb84a9f3d2a53a0871ae011ec9511992591dd1b
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2017-12-12 21:48:29 -0300

    Added ViewModelLocator.cs

---
+++ UniTube.Framework/Mvvm/ViewModelLocator.cs
@@ -0,0 +1,40 @@
+﻿using Windows.UI.Xaml;
+
+namespace UniTube.Framework.Mvvm
+{
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty AutoWireViewModelProperty = DependencyProperty.RegisterAttached(
+            "AutoWireViewModel", typeof(bool), typeof(ViewModelLocator), new PropertyMetadata(false, OnAutoWireViewModelChanged));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool GetAutoWireViewModel(DependencyObject obj) => (bool)obj.GetValue(AutoWireViewModelProperty);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="value"></param>
+        public static void SetAutoWireViewModel(DependencyObject obj, bool value)
+            => obj.SetValue(AutoWireViewModelProperty, value);
+
+        private static void OnAutoWireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+                ViewModelLocationProvider.AutoWireViewModelChanged(d, Bind);
+        }
+
+        private static void Bind(object view, object viewModel)
+        {
+            if (view is FrameworkElement element)
+                element.DataContext = viewModel;
+        }
+    }
+}
