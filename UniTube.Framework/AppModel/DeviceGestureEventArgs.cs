a62fe7b5d45eb30ecd20e9b7b8eb8af88ec88396
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2017-12-12 21:52:06 -0300

    Added IDeviceGestureService.cs

---
+++ UniTube.Framework/AppModel/DeviceGestureEventArgs.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace UniTube.Framework.AppModel
+{
+    public class DeviceGestureEventArgs : CancelEventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DeviceGestureEventArgs() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="handled"></param>
+        public DeviceGestureEventArgs(bool handled)
+        {
+            Handled = handled;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="handled"></param>
+        /// <param name="isHardwareButton"></param>
+        public DeviceGestureEventArgs(bool handled, bool isHardwareButton)
+        {
+            Handled = handled;
+            IsHardwareButton = isHardwareButton;
+        }
+
+        /// <summary>
+        /// Gets
+        /// </summary>
+        public bool IsHardwareButton { get; }
+
+        /// <summary>
+        /// Gets or sets
+        /// </summary>
+        public bool Handled { get; set; }
+    }
+}
---
+++ UniTube.Framework/AppModel/IDeviceGestureService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.Devices.Input;
+
+namespace UniTube.Framework.AppModel
+{
+    public interface IDeviceGestureService
+    {
+        bool IsHardwareBackButtonPresent { get; }
+        bool IsHardwareCameraButtonPresent { get; }
+        bool IsKeyboardPresent { get; }
+        bool IsMousePresent { get; }
+        bool IsTouchPresent { get; }
+        bool UseTitleBarBackButton { get; set; }
+
+        event EventHandler<DeviceGestureEventArgs> CameraButtonHalfPressed;
+        event EventHandler<DeviceGestureEventArgs> CameraButtonPressed;
+        event EventHandler<DeviceGestureEventArgs> CameraButtonReleased;
+        event EventHandler<DeviceGestureEventArgs> GoBackRequested;
+        event EventHandler<DeviceGestureEventArgs> GoForwardRequested;
+        event EventHandler<MouseEventArgs> MouseMoved;
+    }
+}
