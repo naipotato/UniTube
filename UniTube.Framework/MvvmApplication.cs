d870fdaca1c401e002d2d548024253b692009d0a
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2017-12-12 22:21:44 -0300

    Implemented OnResuming() method

--- UniTube.Framework/MvvmApplication.cs
+++ UniTube.Framework/MvvmApplication.cs
@@ -11,6 +11,7 @@ namespace UniTube.Framework
     public abstract class MvvmApplication : Application
     {
         #region Private variables
+        private bool _handledOnResume;
         private object _pageKeys; 
         #endregion
 
@@ -38,13 +39,35 @@ namespace UniTube.Framework
 
         protected INavigationService NavigationService { get; private set; }
 
+        protected bool RestoreNavigationStateOnResume { get; private set; }
         protected ISessionStateService SessionStateService { get; private set; }
         #endregion
 
         #region Methods
+        /// <summary>
+        /// Override this method with logic that will be performed when resuming after the application is initialized.
+        /// For example, refreshing user credentials.
+        /// <para>This method is called after the navigation state is restored, triggering the
+        /// <see cref="ViewModelBase.OnNavigatedTo"/> event on the active view model.</para>
+        /// <para>Note: This is called whenever the app is resuming from suspend and terminate, but not during
+        /// a fresh launch and not when reactivating the app if it hasn't been suspended.</para>
+        /// </summary>
+        /// <param name="args">
+        /// The <see cref="IActivatedEventArgs"/> instance containing the event data if the app is activated. <c>null</c>
+        /// if the app is only suspended and resumed without reactivation.</param>
+        /// <returns>An <see cref="IAsyncAction"/> that represents an asynchronous action.</returns>
+        protected virtual IAsyncAction OnResumeAsync(IActivatedEventArgs args) => Task.CompletedTask.AsAsyncAction();
+
+#pragma warning disable CS4014
         private void OnResuming(object sender, object e)
         {
+            if (RestoreNavigationStateOnResume)
+                NavigationService.RestoreSavedNavigation();
+
+            _handledOnResume = true;
+            OnResumeAsync(null);
         }
+#pragma warning restore CS4014
 
         private async void OnSuspending(object sender, SuspendingEventArgs e)
         {
@@ -76,6 +99,11 @@ namespace UniTube.Framework
         protected virtual IAsyncAction OnSuspendingAsync(object sender, SuspendingEventArgs e)
             => Task.CompletedTask.AsAsyncAction();
 
+        /// <summary>
+        /// Get a key dictionary to link the different pages to an enumerable.
+        /// </summary>
+        /// <typeparam name="T">The enum that represents the keys.</typeparam>
+        /// <returns>The key dictionary.</returns>
         public Dictionary<T, Type> PageKeys<T>() where T : struct, IConvertible
         {
             if (!typeof(T).GetTypeInfo().IsEnum)
