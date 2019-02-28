16d453b90cdfbc5c068b643a3b694df221dae619
nahuelwexd <nahual_gomca@outlook.com.ar>
2019-02-28 00:59:37 -0300

    Documented some properties and methods

--- src/UniTube.Core/Resources/Video.cs
+++ src/UniTube.Core/Resources/Video.cs
@@ -23,24 +23,82 @@ using UniTube.Core.Requests;
 
 namespace UniTube.Core.Resources
 {
+    /// <summary>
+    /// A video resource represents a YouTube video.
+    /// </summary>
     public class Video
     {
+        /// <summary>
+        /// Identifies the API resource's type.
+        /// </summary>
         public string Kind { get; set; }
+
+        /// <summary>
+        /// The Etag of this resource.
+        /// </summary>
         public string Etag { get; set; }
+
+        /// <summary>
+        /// The ID that YouTube uses to uniquely identify the video.
+        /// </summary>
         public string Id { get; set; }
+
+        /// <summary>
+        /// The snippet object contains basic details about the video, such as
+        /// its title, description, and category.
+        /// </summary>
         public VideoSnippet Snippet { get; set; }
 
+        /// <summary>
+        /// Contains basic details about the video, such as its title,
+        /// description, and category.
+        /// </summary>
         public class VideoSnippet
         {
+            /// <summary>
+            /// The date and time that the video was published.
+            /// </summary>
             public DateTime PublishedAt { get; set; }
+
+            /// <summary>
+            /// The ID that YouTube uses to uniquely identify the channel that
+            /// the video was uploaded to.
+            /// </summary>
             public string ChannelId { get; set; }
+
+            /// <summary>
+            /// The video's title.
+            /// </summary>
             public string Title { get; set; }
+
+            /// <summary>
+            /// The video's description.
+            /// </summary>
             public string Description { get; set; }
+
+            /// <summary>
+            /// A map of thumbnail images associated with the video.
+            /// </summary>
             public Thumbnails Thumbnails { get; set; }
+
+            /// <summary>
+            /// Channel title for the channel that the video belongs to.
+            /// </summary>
             public string ChannelTitle { get; set; }
+
+            /// <summary>
+            /// A list of keyword tags associated with the video.
+            /// </summary>
             public List<string> Tags { get; set; }
         }
 
+        /// <summary>
+        /// Returns a list of videos that match the API request parameters.
+        /// </summary>
+        /// <param name="part">Specifies a comma-separated list of one or more
+        /// video resource properties that the API response will include.</param>
+        /// <return>An instance of VideoListRequest to configure the
+        /// request.</return>
         public static VideoListRequest List(string part)
             => new VideoListRequest(part);
     }
