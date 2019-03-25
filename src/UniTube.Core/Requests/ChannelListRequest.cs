9f1579d6d3388e9122aff03d4832589df8c9bfc9
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2019-03-25 19:52:35 -0300

    Requests rewritted from scratch

--- src/UniTube.Core/Requests/ChannelListRequest.cs
+++ src/UniTube.Core/Requests/ChannelListRequest.cs
@@ -1,24 +1,23 @@
 // Copyright (C) 2019 Nucleux Software
-// 
+//
 // This file is part of UniTube.
-// 
+//
 // UniTube is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // UniTube is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with UniTube.  If not, see <https://www.gnu.org/licenses/>.
-// 
+//
 // Author: Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
 
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -26,47 +25,93 @@ using UniTube.Core.Responses;
 
 namespace UniTube.Core.Requests
 {
-    public class ChannelListRequest : RequestBase<ChannelListResponse>
+    public class ChannelListRequest : Request<ChannelListResponse>
     {
-        private string part;
+        public ChannelListRequest(string part) => this.Part = part;
 
-        internal ChannelListRequest(string part) => this.part = part;
+        public string Part
+        {
+            get => this.GetPropertyString("part");
+            private set => this.SetPropertyString("part", value);
+        }
 
         public string CategoryId
         {
-            get => this.pairs.ContainsKey("categoryId") ?
-                this.pairs["categoryId"] : null;
-            set
-            {
-                if (string.IsNullOrWhiteSpace(value))
-                    this.pairs.Remove("categoryId");
-                else
-                    this.pairs["categoryId"] = value;
-            }
+            get => this.GetPropertyString("categoryId");
+            set => this.SetPropertyString("categoryId", value);
         }
 
-        public override async Task<ChannelListResponse> ExecuteAsync()
+        public string ForUsername
         {
-            var parameters = JoinPairs(pairs);
+            get => this.GetPropertyString("forUsername");
+            set => this.SetPropertyString("forUsername", value);
+        }
 
+        public string Id
+        {
+            get => this.GetPropertyString("id");
+            set => this.SetPropertyString("id", value);
+        }
+
+        public bool? ManagedByMe
+        {
+            get => this.GetPropertyBoolean("managedByMe");
+            set => this.SetPropertyBoolean("managedByMe", value);
+        }
+
+        public bool? Mine
+        {
+            get => this.GetPropertyBoolean("mine");
+            set => this.SetPropertyBoolean("mine", value);
+        }
+
+        public string Hl
+        {
+            get => this.GetPropertyString("hl");
+            set => this.SetPropertyString("hl", value);
+        }
+
+        public long MaxResults
+        {
+            get => this.GetPropertyLong("maxResults");
+            set => this.SetPropertyLong("maxResults", value);
+        }
+
+        public string OnBehalfOfContentOwner
+        {
+            get => this.GetPropertyString("onBehalfOfContentOwner");
+            set => this.SetPropertyString("onBehalfOfContentOwner", value);
+        }
+
+        public string PageToken
+        {
+            get => this.GetPropertyString("pageToken");
+            set => this.SetPropertyString("pageToken", value);
+        }
+
+        public async override Task<ChannelListResponse> ExecuteRequestAsync()
+        {
+            var parameters = this.GetParameters();
             var httpClient = new HttpClient();
 
             try
             {
-                var httpResponse = await httpClient.GetAsync($"https://www.googleapis.com/youtube/v3/channels?{parameters}");
-                if (httpResponse.IsSuccessStatusCode)
+                var responseMessage = await httpClient.GetAsync(
+                $"https://www.googleapis.com/youtube.com/v3/channels?{parameters}");
+
+                if (responseMessage.IsSuccessStatusCode)
                 {
-                    var contentResponse = await httpResponse.Content.ReadAsStringAsync();
-                    var channelListResponse = JsonConvert.DeserializeObject<ChannelListResponse>(contentResponse);
-                    return channelListResponse;
+                    var contentResponse = await responseMessage.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<ChannelListResponse>(contentResponse);
                 }
                 else
-                    throw new Exception($"Error {httpResponse.StatusCode}\n{httpResponse.ReasonPhrase}\n{await httpResponse.Content.ReadAsStringAsync()}");
+                    throw new Exception(
+                        $"Error {responseMessage.StatusCode}\n{responseMessage.ReasonPhrase}\n{await responseMessage.Content.ReadAsStringAsync()}");
             }
-            catch (HttpRequestException ex)
-            {
-                throw new Exception("Exception while request", ex);
-            }
+			catch (Exception ex)
+			{
+				throw new Exception("Exception while request", ex);
+			}
         }
     }
 }
--- src/UniTube.Core/Requests/SearchListRequest.cs
+++ src/UniTube.Core/Requests/SearchListRequest.cs
@@ -1,36 +1,219 @@
 // Copyright (C) 2019 Nucleux Software
-// 
+//
 // This file is part of UniTube.
-// 
+//
 // UniTube is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // UniTube is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with UniTube.  If not, see <https://www.gnu.org/licenses/>.
-// 
+//
 // Author: Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
 
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using UniTube.Core.Responses;
 
 namespace UniTube.Core.Requests
 {
-    public class SearchListRequest : RequestBase<SearchListResponse>
+    public class SearchListRequest : Request<SearchListResponse>
     {
-        private string part;
+        public SearchListRequest(string part) => this.Part = part;
 
-        internal SearchListRequest(string part) => this.part = part;
+		public string Part
+		{
+			get => this.GetPropertyString("part");
+			private set => this.SetPropertyString("part", value);
+		}
 
-        public override Task<SearchListResponse> ExecuteAsync()
+		public bool? ForContentOwner
+		{
+			get => this.GetPropertyBoolean("forContentOwner");
+			set => this.SetPropertyBoolean("forContentOwner", value);
+		}
+
+		public bool? ForMine
+		{
+			get => this.GetPropertyBoolean("forMine");
+			set => this.SetPropertyBoolean("forMine", value);
+		}
+
+		public string RelevantToVideoId
+		{
+			get => this.GetPropertyString("relevantToVideoId");
+			set => this.SetPropertyString("relevantToVideoId", value);
+		}
+
+		public string ChannelId
+		{
+			get => this.GetPropertyString("channelId");
+			set => this.SetPropertyString("channelId", value);
+		}
+
+		public string ChannelType
+		{
+			get => this.GetPropertyString("channelType");
+			set => this.SetPropertyString("channelType", value);
+		}
+
+		public string EventType
+		{
+			get => this.GetPropertyString("eventType");
+			set => this.SetPropertyString("eventType", value);
+		}
+
+		public long MaxResults
+		{
+			get => this.GetPropertyLong("maxResults");
+			set => this.SetPropertyLong("maxResults", value);
+		}
+
+		public string OnBehalfOfContentOwner
+		{
+			get => this.GetPropertyString("onBehalfOfContentOwner");
+			set => this.SetPropertyString("onBehalfOfContentOwner", value);
+		}
+
+		public string Order
+		{
+			get => this.GetPropertyString("order");
+			set => this.SetPropertyString("order", value);
+		}
+
+		public string PageToken
+		{
+			get => this.GetPropertyString("pageToken");
+			set => this.SetPropertyString("pageToken", value);
+		}
+
+		public DateTime? PublishedAfter
+		{
+			get => this.GetPropertyDateTime("publishedAfter");
+			set => this.SetPropertyDateTime("publishedAfter", value);
+		}
+
+		public DateTime? PublishedBefore
+		{
+			get => this.GetPropertyDateTime("publishedBefore");
+			set => this.SetPropertyDateTime("publishedBefore", value);
+		}
+
+		public string Q
+		{
+			get => this.GetPropertyString("q");
+			set => this.SetPropertyString("q", value);
+		}
+
+		public string RegionCode
+		{
+			get => this.GetPropertyString("regionCode");
+			set => this.SetPropertyString("regionCode", value);
+		}
+
+		public string SafeSearch
+		{
+			get => this.GetPropertyString("safeSearch");
+			set => this.SetPropertyString("safeSearch", value);
+		}
+
+		public string TopicId
+		{
+			get => this.GetPropertyString("topicId");
+			set => this.SetPropertyString("topicId", value);
+		}
+
+		public string Type
+		{
+			get => this.GetPropertyString("type");
+			set => this.SetPropertyString("type", value);
+		}
+
+		public string VideoCaption
+		{
+			get => this.GetPropertyString("videoCaption");
+			set => this.SetPropertyString("videoCaption", value);
+		}
+
+		public string VideoCategoryId
+		{
+			get => this.GetPropertyString("videoCategoryId");
+			set => this.SetPropertyString("videoCategoryId", value);
+		}
+
+		public string VideoDefinition
+		{
+			get => this.GetPropertyString("videoDefinition");
+			set => this.SetPropertyString("videoDefinition", value);
+		}
+
+		public string VideoDimension
+		{
+			get => this.GetPropertyString("videoDimension");
+			set => this.SetPropertyString("videoDimension", value);
+		}
+
+		public string VideoDuration
+		{
+			get => this.GetPropertyString("videoDuration");
+			set => this.SetPropertyString("videoDuration", value);
+		}
+
+		public string VideoEmbeddable
+		{
+			get => this.GetPropertyString("videoEmbeddable");
+			set => this.SetPropertyString("videoEmbeddable", value);
+		}
+
+		public string VideoLicense
+		{
+			get => this.GetPropertyString("videoLicense");
+			set => this.SetPropertyString("videoLicense", value);
+		}
+
+		public string VideoSyndicated
+		{
+			get => this.GetPropertyString("videoSyndicated");
+			set => this.SetPropertyString("videoSyndicated", value);
+		}
+
+		public string VideoType
+		{
+			get => this.GetPropertyString("videoType");
+			set => this.SetPropertyString("videoType", value);
+		}
+
+        public async override Task<SearchListResponse> ExecuteRequestAsync()
         {
-            throw new System.NotImplementedException();
+            var parameters = this.GetParameters();
+            var httpClient = new HttpClient();
+
+            try
+            {
+                var responseMessage = await httpClient.GetAsync(
+                $"https://www.googleapis.com/youtube.com/v3/search?{parameters}");
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var contentResponse = await responseMessage.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<SearchListResponse>(contentResponse);
+                }
+                else
+                    throw new Exception(
+                        $"Error {responseMessage.StatusCode}\n{responseMessage.ReasonPhrase}\n{await responseMessage.Content.ReadAsStringAsync()}");
+            }
+			catch (Exception ex)
+			{
+				throw new Exception("Exception while request", ex);
+			}
         }
     }
 }
