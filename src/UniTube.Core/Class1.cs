c813fbd014d4342599cb83f59ddd747c6730ab3f
Nahuel wexd <nahual_gomca@outlook.com.ar>
2019-05-18 16:17:27 -0300

    Started with the video resource's implementation

--- src/UniTube.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace UniTube.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/UniTube.Core/Data/VideoSnippet.cs
@@ -0,0 +1,50 @@
+// UniTube - An open source client for YouTube
+// Copyright (C) 2019 Nucleux Software
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Author: Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
+
+using System;
+using System.Collections.Generic;
+
+namespace UniTube.Core.Data
+{
+    public class VideoSnippet
+    {
+        public DateTime PublishedAt { get; set; }
+
+        public string ChannelId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Dictionary<string, Thumbnail> Thumbnails { get; set; }
+
+        public string ChannelTitle { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public string LiveBroadcastContent { get; set; }
+
+        public string DefaultLanguage { get; set; }
+
+        public VideoLocalization Localized { get; set; }
+
+        public string DefaultAudioLanguage { get; set; }
+    }
+}
