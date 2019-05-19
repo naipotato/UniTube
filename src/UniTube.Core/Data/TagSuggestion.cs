8e5cc2614fb58fc3a47f0d96d7196414ab81b115
Nahuel wexd <nahual_gomca@outlook.com.ar>
2019-05-19 13:11:23 -0300

    Added Suggestions object to video resource

--- src/UniTube.Core/Data/TagSuggestion.cs
+++ src/UniTube.Core/Data/TagSuggestion.cs
@@ -16,34 +16,14 @@
 //
 // Author: Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
 
+using System.Collections.Generic;
+
 namespace UniTube.Core.Data
 {
-    public class Video
+    public class TagSuggestion
     {
-        public string Kind { get; set; }
+        public string Tag { get; set; }
 
-        public string Etag { get; set; }
-
-        public string Id { get; set; }
-
-        public VideoSnippet Snippet { get; set; }
-
-        public VideoContentDetails ContentDetails { get; set; }
-
-        public VideoStatus Status { get; set; }
-
-        public VideoStatistics Statistics { get; set; }
-
-        public VideoPlayer Player { get; set; }
-
-        public VideoTopicDetails TopicDetails { get; set; }
-
-        public VideoRecordingDetails RecordingDetails { get; set; }
-
-        public VideoFileDetails FileDetails { get; set; }
-
-        public VideoProcessingDetails ProcessingDetails { get; set; }
-
-        public VideoSuggestions Suggestions { get; set; }
+        public List<string> CategoryRestricts { get; set; }
     }
 }
--- src/UniTube.Core/Data/VideoSuggestions.cs
+++ src/UniTube.Core/Data/VideoSuggestions.cs
@@ -16,34 +16,20 @@
 //
 // Author: Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
 
+using System.Collections.Generic;
+
 namespace UniTube.Core.Data
 {
-    public class Video
+    public class VideoSuggestions
     {
-        public string Kind { get; set; }
+        public List<string> ProcessingErrors { get; set; }
 
-        public string Etag { get; set; }
+        public List<string> ProcessingWarnings { get; set; }
 
-        public string Id { get; set; }
+        public List<string> ProcessingHints { get; set; }
 
-        public VideoSnippet Snippet { get; set; }
+        public List<TagSuggestion> TagSuggestions { get; set; }
 
-        public VideoContentDetails ContentDetails { get; set; }
-
-        public VideoStatus Status { get; set; }
-
-        public VideoStatistics Statistics { get; set; }
-
-        public VideoPlayer Player { get; set; }
-
-        public VideoTopicDetails TopicDetails { get; set; }
-
-        public VideoRecordingDetails RecordingDetails { get; set; }
-
-        public VideoFileDetails FileDetails { get; set; }
-
-        public VideoProcessingDetails ProcessingDetails { get; set; }
-
-        public VideoSuggestions Suggestions { get; set; }
+        public List<string> EditorSuggestions { get; set; }
     }
 }
